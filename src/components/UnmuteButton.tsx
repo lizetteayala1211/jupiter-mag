43a25a663620475073a2ba8ff0f9622c614935c3
Nishat Firoj <nishat.firoj@gmail.com>
2023-12-12 16:58:33 -0500

    more duplicitious stuff

--- src/components/UnmuteButton.tsx
+++
@@ -1,6 +0,0 @@
-"use client"
-import React from "react"
-
-export function UnmuteButton({ unmuteCallback }: { unmuteCallback: any }) {
-  return <UnmuteButton onClick={unmuteCallback}>Mute</UnmuteButton>
-}
