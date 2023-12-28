9e8dec90b0b208d4527cd9cbc1e08b30c3d7d743
Nishat Firoj <nishat.firoj@gmail.com>
2023-12-27 19:52:25 -0500

    set up about page with real and fake content lol, still need to fix video per their specifications

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,19 +4,59 @@ import { Base } from "@/components/base/Base"
 import {
   CaptionSection,
   DescSection,
-  HeroSection,
+  VideoSection,
   Main,
   PhotoSection,
 } from "./styled"
+import { Stream } from "@cloudflare/stream-react"
+import React from "react"
 
 export default function About() {
+  const [userInteracted, setUserInteracted] = React.useState(false)
+
   return (
     <Base>
-      <Main>
-        <HeroSection />
-        <CaptionSection />
-        <PhotoSection />
-        <DescSection />
+      <Main onClick={() => setUserInteracted(true)}>
+        <VideoSection>
+          {" "}
+          <Stream
+            src="1f0b81560d3380d4b27e3d2acf8d4e00"
+            autoplay
+            loop
+            muted={!userInteracted}
+          />
+        </VideoSection>
+        <CaptionSection>
+          Video caption/ credits. ed ut perspiciatis unde omnis iste natus error
+          sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
+          eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae
+          vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+          sit aspernatur aut emnis iste natus error sit voluptatem accusantium
+          odit aut quia voluptas sit aspernatur aut emnis iste natus error
+          fugit.{" "}
+        </CaptionSection>
+        <PhotoSection>
+          <div
+            style={{ width: "410px", height: "527px", backgroundColor: "grey" }}
+          >
+            dummy photo
+          </div>
+        </PhotoSection>
+        <DescSection>
+          Jupiter Magazine is a quarterly arts and culture publication that
+          marks a restorative departure into an editorial space that is a nexus
+          of political consciousness, spiritual awareness, and creative healing.
+          Grounded in expressive care and expanded with cosmic phenomenology,
+          Jupiter began as a prayer, a spell, and an incantation—a belief that a
+          world could be built through naming a desire. As two writers working
+          within, but venturing beyond, the restrictive systems of the art world
+          and the constraining vocabulary of art criticism, co-founders Camille
+          Gallogly Bacon and Daria Simone Harper came together first through an
+          admiration for each other&apos;s work, and have since co-conspired
+          towards the building of Jupiter: a publication invested in disrupting
+          industry standards in service of creating conditions that support more
+          viable writing lives.
+        </DescSection>
       </Main>
     </Base>
   )
--- src/app/about/styled.ts
+++ src/app/about/styled.ts
@@ -3,30 +3,40 @@ import styled from "styled-components"
 
 export const Main = styled(SharedGridMain)`
   grid-template-areas:
-    "hero"
+    "video"
     "caption"
     "photo"
     "desc";
   grid-template-columns: 5fr 5fr;
   grid-template-rows: 6fr 1fr 4fr;
   grid-template-areas:
-    "hero hero"
+    "video video"
     "caption caption"
     "photo desc";
 `
 
-export const HeroSection = styled.section`
-  grid-area: hero;
+export const VideoSection = styled.section`
+  grid-area: video;
+  height: auto;
+  width: 100%;
 `
 
 export const CaptionSection = styled.section`
   grid-area: caption;
+  padding: 0 4em;
 `
 
 export const PhotoSection = styled.section`
   grid-area: photo;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 `
 
 export const DescSection = styled.section`
   grid-area: desc;
+  padding: 0 4em;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 `
