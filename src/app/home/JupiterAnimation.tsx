fcc22f26f4ba48f540da611747b55357e9949b33
Nishat Firoj <nishat.firoj@gmail.com>
2024-01-12 12:15:24 -0500

    new lottie dependency that does not cause server/client issues and also cleaned up author background svg

--- src/app/home/JupiterAnimation.tsx
+++ src/app/home/JupiterAnimation.tsx
@@ -1,16 +1,27 @@
 import React from "react"
-import "@lottiefiles/lottie-player"
+import type { LottiePlayer } from "lottie-web"
 
 export function JupiterAnimation() {
-  const ref: React.Ref<any> = React.useRef(null)
+  const ref: React.Ref<any> = React.useRef<HTMLDivElement>(null)
+  const [lottie, setLottie] = React.useState<LottiePlayer | null>(null)
 
-  return (
-    <lottie-player
-      ref={ref}
-      autoplay
-      loop
-      mode="normal"
-      src="https://lottie.host/8c7bf9d6-1300-4850-ada7-b44028bfb70d/rwAUsMzGIB.json"
-    />
-  )
+  React.useEffect(() => {
+    import("lottie-web").then((Lottie) => setLottie(Lottie.default))
+  }, [])
+
+  React.useEffect(() => {
+    if (lottie && ref.current) {
+      const animation = lottie.loadAnimation({
+        container: ref.current,
+        renderer: "svg",
+        loop: true,
+        autoplay: true,
+        path: "/static/jupiter.lottie.json",
+      })
+
+      return () => animation.destroy()
+    }
+  }, [lottie])
+
+  return <div ref={ref} />
 }
