1e61891f78776af82114281b7d1a0b4fa7847635
Nishat Firoj <nishat.firoj@gmail.com>
2024-01-21 19:30:20 -0500

    correct copy for contact page

--- src/app/contact/desktop/index.tsx
+++ src/app/contact/desktop/index.tsx
@@ -7,21 +7,19 @@ export default function Desktop() {
     <Main>
       <ContactSection>
         <div>
-          <h1 style={{ letterSpacing: "4px" }}>Daria Harper</h1>
-          <h2 style={{ letterSpacing: "4px" }}>Founder</h2>
-          <h3>Dariaharper@gmail.com</h3>
+          <h1>Daria Harper</h1>
+          <h2>Founder</h2>
         </div>
         <div>
-          <h1 style={{ letterSpacing: "4px" }}>Camille Bacon </h1>
-          <h2 style={{ letterSpacing: "4px" }}>Founder</h2>
-          <h3>camillebacon@gmail.com</h3>
+          <h1>Camille Bacon </h1>
+          <h2>Founder</h2>
         </div>
         <div>
           <h1 style={{ fontFamily: "Alverata Light" }}>Email</h1>
           <h2>info@jupiter-mag.com</h2>
         </div>
         <div>
-          <h2 style={{ letterSpacing: "4px" }}>Instagram</h2>
+          <h2>Instagram</h2>
           <h3>@jupiter.magazine</h3>
         </div>
         <p style={{ maxWidth: "300px" }}>
