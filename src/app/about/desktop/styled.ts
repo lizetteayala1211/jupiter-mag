1d400968a8933f121eb65dd4efb880800ca48237
Nishat Firoj <nishat.firoj@gmail.com>
2024-03-12 19:40:22 -0400

    removed about pic and desc

--- src/app/about/desktop/styled.ts
+++ src/app/about/desktop/styled.ts
@@ -9,9 +9,7 @@ export const Main = styled(SharedGridMain)`
   grid-template-columns: 5fr 5fr;
   grid-template-rows: repeat(auto-fill, 1fr);
 
-  grid-template-areas:
-    "video video"
-    "photo desc";
+  grid-template-areas: "video video";
 `
 
 export const VideoSection = styled.section`
@@ -19,20 +17,4 @@ export const VideoSection = styled.section`
   height: auto;
   width: 100%;
   margin-bottom: 128px;
-`
-
-export const PhotoSection = styled.section`
-  grid-area: photo;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  margin: var(--padding-body);
-`
-
-export const DescSection = styled.section`
-  grid-area: desc;
-  margin: 4em 8em 4em 4em;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
 `
