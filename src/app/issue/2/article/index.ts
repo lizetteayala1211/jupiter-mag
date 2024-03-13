5b67cbd432a64ec8dd134bd976f056a5ca14f1b2
Nishat Firoj <nishat.firoj@gmail.com>
2024-03-12 22:30:07 -0400

    added first bit of metadata for issue 2

---
+++ src/app/issue/2/article/index.ts
@@ -0,0 +1,25 @@
+import { ArticleMetadata } from "@/utils/types"
+
+// future: have this data hosted on a db, instead of on client
+export const issue2Metadata: ArticleMetadata[] = [
+  {
+    title: "Endurance at an Angle",
+    link: "/issue/2/article/zoe",
+    author: "Zoë Hopkins",
+  },
+  {
+    title: "Outlive",
+    link: "/issue/2/article/alexis",
+    author: "Alexis Pauline Gumbs",
+  },
+  {
+    title: "In Absentia",
+    link: "/issue/2/article/isra",
+    author: "Isra Rene",
+  },
+  {
+    title: "todo: article_title",
+    link: "/issue/2/article/rohan",
+    author: "Rohan Ayinde",
+  },
+]
