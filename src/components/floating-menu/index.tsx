485952a6a5a1b1fa05fd1aeca2a12d060477e628
Lizette Ayala <ayall291@newschool.edu>
2024-06-07 14:22:07 -0400

    Add files via upload

--- src/components/floating-menu/index.tsx
+++ src/components/floating-menu/index.tsx
@@ -3,24 +3,43 @@ import { FloatingMenuContainer } from "./styled"
 type FloatingMenuProps = { issue: number }
 
 export function FloatingMenu({ issue }: FloatingMenuProps) {
+  // const pastIssues =
   return (
     <FloatingMenuContainer>
       <li style={{ fontSize: "24px", textTransform: "none" }}>Issue {issue}</li>
       <li>
         <a href="#editorsNote">Editor&apos;s note</a>
       </li>
-      <li>
-        <a href="#articles">Articles</a>
-      </li>
-      <li>
-        <a href="#writers">Writers</a>
-      </li>
+      {issue != 3 ? <>
+        <li>
+          <a href="#articles">Articles</a>
+        </li>
+        <li>
+          <a href="#writers">Writers</a>
+        </li>
+      </> : <li>
+          <a href="#programs">Programs</a>
+        </li>}
       <li>
         <a href="#colophon">Colophon</a>
       </li>
-      <li>
-        <a href="/issue/1">Issue {issue === 1 ? 2 : 1}</a>
-      </li>
+      {createPastIssues(issue).map(i => (
+        <li key={`/issue/${i}`}>
+          <a href={`/issue/${i}`}>Issue {i}</a>
+        </li>
+      ))
+      }
     </FloatingMenuContainer>
   )
+}
+
+const createPastIssues = (issue: number) => {
+  const pastIssues = []
+
+  for (let i = 1; i < issue ; i++) {
+    pastIssues.push(i)
+  }
+
+  return pastIssues.reverse()
+
 }
