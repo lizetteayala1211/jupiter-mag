71b1e3afbf4760d653a8ab7213b98ebdff44145e
Lizette Ayala <lizetteayala1211@gmail.com>
2025-01-16 09:06:40 -0500

    text color update - rianna, issue 1

--- src/app/issue/1/article/rianna/article.tsx
+++ src/app/issue/1/article/rianna/article.tsx
@@ -13,8 +13,8 @@ export function Article() {
         alt="Black cultural artwork"
       />
       <CaptionContainer>
-        <span>Albert Artwell, </span>
-        <i> Jesse through the Black Star Liner (c. 1980s).</i> Sea with four
+        <span style={{ color: "white" }}>Albert Artwell, </span>
+        <i style={{ color: "white" }}> Jesse through the Black Star Liner (c. 1980s).</i> Sea with four
         boats. Time is collapsed with boats carrying Jesse, Jesus, and other
         biblical figures alongside Marcus Garvey&apos;s Black Star Liner.
       </CaptionContainer>
@@ -24,15 +24,15 @@ export function Article() {
         </li>
       </ol>
       <p className="c5">
-        <span className="c1">
+        <span className="c1" style={{ color: "white" }}>
           The largest Black political organ of the twentieth century, the
           Universal Negro Improvement Association (UNIA), was co-founded by A
         </span>
-        <span className="c1">
+        <span className="c1" style={{ color: "white" }}>
           my Ashwood Garvey and Marcus Mosiah Garvey in Kingston, Jamaica amidst
           a range
         </span>
-        <span className="c0">
+        <span className="c0" style={{ color: "white" }}>
           &nbsp;of self-organizing institutions of the laboring class, opting to
           advance their rights and interests as workers and citizens. In 1917,
           the UNIA headquarters was relocated to the Black Metropolis of Harlem,
@@ -46,12 +46,12 @@ export function Article() {
         <span className="c0"></span>
       </p>
       <p className="c5">
-        <span className="c1">
+        <span className="c1" style={{ color: "white" }}>
           While the UNIA&rsquo;s enterprises and aspirations included an
           internationally distributed newspaper,{" "}
         </span>
-        <span className="c1 c3">The Negro World</span>
-        <span className="c0">
+        <span className="c1 c3" style={{ color: "white" }}>The Negro World</span>
+        <span className="c0" style={{ color: "white" }}>
           , the main economic venture was an all-Black shipping company
           incorporated as the Black Star Line: Owned, operated, and financed by
           Black people. Although it suffered detrimentally due to mismanagement
@@ -69,7 +69,7 @@ export function Article() {
         </li>
       </ol>
       <p className="c5">
-        <span className="c0">
+        <span className="c0" style={{ color: "white" }}>
           The imaginings of Albert Artwell, one of Jamaica&rsquo;s most
           prominent self-taught artists, were spun with biblical stories, town
           scenes, and seascapes. Adamant that God is Black and that Jesus had
@@ -86,38 +86,38 @@ export function Article() {
         <span className="c0"></span>
       </p>
       <p className="c5">
-        <span className="c1 c3">
+        <span className="c1 c3" style={{ color: "white" }}>
           Jesse through the Black Star Liner (c. 1980s)
         </span>
-        <span className="c1">
+        <span className="c1" style={{ color: "white" }}>
           &nbsp;not only contests and confronts the dominant cosmogonic model,
           but also proposes an alternative.
         </span>
-        <span className="c1">
+        <span className="c1" style={{ color: "white" }}>
           &nbsp;Artwell&rsquo;s color choices privileged the Rastafari hues of
           red, green, and gold. Flat-painted and illustrative,
         </span>
-        <span className="c4 c8">&nbsp;</span>
-        <span className="c1">
+        <span className="c4 c8" style={{ color: "white" }}>&nbsp;</span>
+        <span className="c1" style={{ color: "white" }}>
           the artwork uses the illusive technique of a stacked perspective,
           reminiscent of Egyptian art. Eschewing the need for
           &ldquo;realistic&rdquo; scale and perspective, the composition and
           symbolism created an alternate version of the world. In this{" "}
         </span>
-        <span className="c1">artwork,</span>
-        <span className="c4 c8">&nbsp;</span>
-        <span className="c1">
+        <span className="c1" style={{ color: "white" }}>artwork,</span>
+        <span className="c4 c8" style={{ color: "white" }}>&nbsp;</span>
+        <span className="c1" style={{ color: "white" }}>
           Jesse, the father of King David, is the central figure. As a man of
           apparent wealth and position in Bethlehem, Jesse is commandeering his
           audience who are lined along the shores, as well as the passengers
           sailing on a Black Star Line ship.{" "}
         </span>
-        <span className="c1">
+        <span className="c1" style={{ color: "white" }}>
           This scene foretells the coming of a Redeemer who{" "}
         </span>
-        <span className="c1">water-bends in order </span>
-        <span className="c1">to </span>
-        <span className="c1">remake.</span>
+        <span className="c1" style={{ color: "white" }}>water-bends in order </span>
+        <span className="c1" style={{ color: "white" }}>to </span>
+        <span className="c1" style={{ color: "white" }}>remake.</span>
       </p>
       <p className="c5 c7">
         <span className="c0"></span>
@@ -128,7 +128,7 @@ export function Article() {
         </li>
       </ol>
       <p className="c5">
-        <span className="c1">
+        <span className="c1" style={{ color: "white" }}>
           One approach to the content, form, and structure of the Black
           aesthetic is a totality of social commitments, cultural values, and
           conventional wisdom. This approach considers the syndrome of internal
@@ -136,52 +136,52 @@ export function Article() {
           of a work of art. Our evaluation is a study of means rather than ends,
           because the message is only part of the poem and part of its success.{" "}
         </span>
-        <span className="c1">After all, p</span>
-        <span className="c1">
+        <span className="c1" style={{ color: "white" }}>After all, p</span>
+        <span className="c1" style={{ color: "white" }}>
           eople learn what is beautiful in the same way they learn language.
         </span>
-        <span className="c1">&nbsp;</span>
+        <span className="c1" style={{ color: "white" }}>&nbsp;</span>
       </p>
       <p className="c5 c7">
         <span className="c12 c3 c14"></span>
       </p>
       <p className="c5">
-        <span className="c12">
+        <span className="c12" style={{ color: "white" }}>
           &ldquo;Culture&rdquo; is the inescapable umbrella term that covers
           almost everything and is still not enough, but just as life, it can
           transcend the matter from which it springs.{" "}
         </span>
-        <span className="c1">
+        <span className="c1" style={{ color: "white" }}>
           Often concerned with joy and justice, there is a strong desire for
           realisms in Black art, but that doesn&rsquo;t rule out euphemism and a
           sense of propriety.
         </span>
-        <span className="c1">&nbsp;</span>
-        <span className="c1">The hegemony of a single globalizing </span>
-        <span className="c1">
+        <span className="c1" style={{ color: "white" }}>&nbsp;</span>
+        <span className="c1" style={{ color: "white" }}>The hegemony of a single globalizing </span>
+        <span className="c1" style={{ color: "white" }}>
           Western culture aids and abets the suppression of existing traditions,
           often through the manipulation of images and narratives over hundreds
           of years. And so, in self-defense there is a constant and{" "}
         </span>
-        <span className="c1">
+        <span className="c1" style={{ color: "white" }}>
           urgent need for cultural counter-missiles.<sup>[a]</sup>
         </span>
 
-        <span className="c1">&nbsp;</span>
-        <span className="c1 c3">Jesse through the Black Star Liner</span>
-        <span className="c1">
+        <span className="c1" style={{ color: "white" }}>&nbsp;</span>
+        <span className="c1 c3" style={{ color: "white" }}>Jesse through the Black Star Liner</span>
+        <span className="c1" style={{ color: "white" }}>
           {" "}
           offers one such oppositional approach through Artwell&rsquo;s
           sensibilities in compositions, cosmologies, and{" "}
         </span>
-        <span className="c1">color</span>
-        <span className="c1">. </span>
+        <span className="c1" style={{ color: "white" }}>color</span>
+        <span className="c1" style={{ color: "white" }}>. </span>
       </p>
       <p className="c5 c7">
         <span className="c0"></span>
       </p>
       <p className="c5">
-        <span className="c1">
+        <span className="c1" style={{ color: "white" }}>
           The Black aesthetic transmutes with time as well as geography. It
           mirrors the Garvey-era shift in socio-political alignment, achieved
           through travel and self-study and produced enduring transatlantic
@@ -191,7 +191,7 @@ export function Article() {
           Overarching this is a whole process of living, ruling oneself, working
           for oneself, feeding and entertaining oneself as a matter of life.{" "}
         </span>
-        <span className="c0">
+        <span className="c0" style={{ color: "white" }}>
           An inward stretch into our already existing depositories of data,
           methods, and processes will best guarantee another 100 years of Black
           aesthetics that reach.
