2ca00b1e83a8466001592cbcf5f72f8857176cd9
Lizette Ayala <lizetteayala1211@gmail.com>
2025-01-30 11:51:16 -0600

    Issue 01 - Diallo Margin Fix (Desktop)

--- src/app/issue/1/article/diallo/article.tsx
+++ src/app/issue/1/article/diallo/article.tsx
@@ -8,7 +8,7 @@ import { CaptionContainer } from "@/utils/layout"
 
 export function Article() {
   return (
-    <ArticleStyles style={{ padding: "0 2em" }}>
+    <ArticleStyles style={{ padding: "0px 10%" }}>
       <br />
 
       <p className="c6">
