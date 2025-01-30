655525ea9e00fb9b6056f5b06f6edbe822a750cc
Lizette Ayala <lizetteayala1211@gmail.com>
2025-01-30 11:21:42 -0600

    hide background grain function

--- src/components/GrainBackgrounds.tsx
+++ src/components/GrainBackgrounds.tsx
@@ -68,62 +68,62 @@ export function DesktopGrain() {
   )
 }
 
-export function MobileGrain() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      version="1.1"
-      viewBox="0 0 700 700"
-      opacity="1"
-      width={1000}
-      height={1000}
-    >
-      <defs>
-        <filter
-          id="nnnoise-filter"
-          x="-20%"
-          y="-20%"
-          width="140%"
-          height="200%"
-          filterUnits="objectBoundingBox"
-          primitiveUnits="userSpaceOnUse"
-          colorInterpolationFilters="linearRGB"
-        >
-          <feTurbulence
-            type="fractalNoise"
-            baseFrequency={mobileBaseFrequency}
-            numOctaves={numOctaves}
-            seed="15"
-            stitchTiles="stitch"
-            x="0%"
-            y="0%"
-            width="100%"
-            height="100%"
-            result="turbulence"
-          ></feTurbulence>
-          <feSpecularLighting
-            surfaceScale={surfaceScale}
-            specularConstant={specularConstant}
-            specularExponent={specularExponent}
-            lightingColor={lightingColor}
-            x="0%"
-            y="0%"
-            width="100%"
-            height="100%"
-            in="turbulence"
-            result="specularLighting"
-          >
-            <feDistantLight azimuth="3" elevation="95"></feDistantLight>
-          </feSpecularLighting>
-        </filter>
-      </defs>
-      <rect width="700" height="700" fill="transparent"></rect>
-      <rect
-        width="700"
-        height="700"
-        fill="#a1a1a1"
-        filter="url(#nnnoise-filter)"
-      ></rect>
-    </svg>
-  )
-}
+// export function MobileGrain() {
+//   return (
+//     <svg
+//       xmlns="http://www.w3.org/2000/svg"
+//       version="1.1"
+//       viewBox="0 0 700 700"
+//       opacity="1"
+//       width={1000}
+//       height={1000}
+//     >
+//       <defs>
+//         <filter
+//           id="nnnoise-filter"
+//           x="-20%"
+//           y="-20%"
+//           width="140%"
+//           height="200%"
+//           filterUnits="objectBoundingBox"
+//           primitiveUnits="userSpaceOnUse"
+//           colorInterpolationFilters="linearRGB"
+//         >
+//           <feTurbulence
+//             type="fractalNoise"
+//             baseFrequency={mobileBaseFrequency}
+//             numOctaves={numOctaves}
+//             seed="15"
+//             stitchTiles="stitch"
+//             x="0%"
+//             y="0%"
+//             width="100%"
+//             height="100%"
+//             result="turbulence"
+//           ></feTurbulence>
+//           <feSpecularLighting
+//             surfaceScale={surfaceScale}
+//             specularConstant={specularConstant}
+//             specularExponent={specularExponent}
+//             lightingColor={lightingColor}
+//             x="0%"
+//             y="0%"
+//             width="100%"
+//             height="100%"
+//             in="turbulence"
+//             result="specularLighting"
+//           >
+//             <feDistantLight azimuth="3" elevation="95"></feDistantLight>
+//           </feSpecularLighting>
+//         </filter>
+//       </defs>
+//       <rect width="700" height="700" fill="transparent"></rect>
+//       <rect
+//         width="700"
+//         height="700"
+//         fill="#a1a1a1"
+//         filter="url(#nnnoise-filter)"
+//       ></rect>
+//     </svg>
+//   )
+// }
