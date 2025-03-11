00d96cba17d58e6f24dd71d8f78d5bde5dfe8fb9
Lizette Ayala <lizetteayala1211@gmail.com>
2025-03-11 17:59:58 -0500

    Tryinng to push all content for Issue 5, amended author directory

--- src/app/issue/5/AuthorDirectory.tsx
+++ src/app/issue/5/AuthorDirectory.tsx
@@ -1,1 +1,19 @@
+import { issue5Metadata } from "@/app/issue/5/article"
+import { StyledAuthorLink, AuthorDirectoryContainer } from "./styled"
 
+export function AuthorDirectory({ homepage }: { homepage?: boolean }) {
+  const isHomePage = homepage!! ? homepage : false
+  return (
+    <AuthorDirectoryContainer>
+      {issue5Metadata.map((author) => (
+        <StyledAuthorLink
+          key={author.author}
+          $homepage={isHomePage.toString()}
+          href={`/issue/5/article/${author.author.split(" ")[0].toLowerCase()}`}
+        >
+          {author.author}
+        </StyledAuthorLink>
+      ))}
+    </AuthorDirectoryContainer>
+  )
+}
