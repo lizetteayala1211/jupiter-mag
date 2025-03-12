24ee6771adb005c37fa9aad4ccc5db800d20fe50
Lizette Ayala <lizetteayala1211@gmail.com>
2025-03-12 10:44:55 -0500

    Push updated Issue 5 landing video

--- src/app/issue/5/page.tsx
+++ src/app/issue/5/page.tsx
@@ -36,7 +36,7 @@ export default function Page() {
       <Main color="black">
         <LottieSection>
           <div className="video-container">
-            <video width="560" height="315" src="../videos/Jupiter_video_off_white_1920x1080.mp4" muted playsInline loop={true} data-autoplay="" autoPlay={true} ></video>
+            <video width="560" height="315" src="../videos/Jupiter_Issue5_Cover_04_web-edit.mp4" muted playsInline loop={true} data-autoplay="" autoPlay={true} ></video>
           </div>
         </LottieSection>
         <LogoSection>
